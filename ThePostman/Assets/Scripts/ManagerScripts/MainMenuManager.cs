a6bc027751e053ec42e19227a06e2c25cfe7d217
cloneraul <raul.marques@escolar.ifrn.edu.br>
2026-09-01 11:07:20 -0300

    creditos adicionados

--- ThePostman/Assets/Scripts/ManagerScripts/MainMenuManager.cs
+++ ThePostman/Assets/Scripts/ManagerScripts/MainMenuManager.cs
@@ -1,12 +1,38 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    
+    [Header("Cenas")]
+    public string primeiraFase = "Fase1";
+
+    [Header("Painéis")]
+    public GameObject panelMenu;
+    public GameObject panelCreditos;
+
+    private void Start()
+    {
+        MostrarMenu();
+    }
+
     public void PlayGame()
     {
-        
-        SceneManager.LoadScene("Fase1"); 
+        SceneManager.LoadScene(primeiraFase);
+    }
+
+    public void AbrirCreditos()
+    {
+        panelMenu.SetActive(false);
+        panelCreditos.SetActive(true);
+    }
+
+    public void MostrarMenu()
+    {
+        panelMenu.SetActive(true);
+        panelCreditos.SetActive(false);
     }
 }
+
+
+
+
