e049abe63c1ee2647726410421bcd7b7b9d97617
cloneraul <raul.marques@escolar.ifrn.edu.br>
2026-09-03 15:44:40 -0300

    espinho adicionado

---
+++ ThePostman/Assets/Scripts/OtherScripts/Spike.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
